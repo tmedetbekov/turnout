a82311065943b8e2b21b9add0b27ed73c02c504b
Talgat <talgat.medetbekov@geopay.kg>
2012-09-25 15:15:03 +0600

    Handled response to json and xml requests

--- lib/rack/turnout.rb
+++ lib/rack/turnout.rb
@@ -2,6 +2,7 @@ require 'rack'
 require 'yaml'
 require 'ipaddr'
 require 'nokogiri'
+require "builder"
 
 class Rack::Turnout
 
@@ -12,19 +13,25 @@ class Rack::Turnout
 
   def call(env)
     reload_settings
-  
+
     if on?(env)
-      [ 503, { 'Content-Type' => 'text/html', 'Content-Length' => content_length }, [content] ]
+      if Rails.application.class.to_s.split("::")[0] == 'MobilnikConnector' && env['CONTENT_TYPE'] == 'application/xml'
+        [200, {'Content-Type' => 'application/xml'}, [xml_content(env['action_dispatch.request.request_parameters'])]]
+      elsif Rails.application.class.to_s.split("::")[0] == 'GeopayUssdConnector' && env['CONTENT_TYPE'] == 'application/json'
+        [200, {'Content-Type' => 'application/json'}, [json_content]]
+      else
+        [503, {'Content-Type' => 'text/html', 'Content-Length' => content_length}, [content]]
+      end
     else
       @app.call(env)
     end
   end
 
   protected
-  
+
   def on?(env)
     request = Rack::Request.new(env)
-    
+
     return false if path_allowed?(request.path)
     return false if ip_allowed?(request.ip)
     File.exists? settings_file
@@ -86,14 +93,36 @@ class Rack::Turnout
 
   def content
     content = File.open(maintenance_page, 'rb').read
-    
+
     if settings['reason']
       html = Nokogiri::HTML(content)
       html.at_css('#reason').inner_html = settings['reason']
       content = html.to_s
     end
-    
+
     content
+  end
+
+  def json_content
+    {
+      data: settings['reason'],
+      last_message: true
+    }.to_json
+  end
+
+  def xml_content(xml)
+    message = {}
+    message[:qid] = xml["XML"]["HEAD"]["QID"]
+    message[:op]= xml["XML"]["HEAD"]["OP"]
+    message[:dts]= Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    message[:reason] = settings["reason"] || "Down for maintenance, please try again later"
+
+    build = Builder::XmlMarkup.new
+    build.instruct! :xml, :version => "1.0", :encoding => "windows-1251"
+    build.XML {
+      build.HEAD("OP" => message[:op], "DTS" => message[:dts], "QID" => message[:qid])
+      build.BODY("STATUS" => "1", "MSG" => settings["reason"])
+    }
   end
 
 end
